770aec8195bdf0b37a763c8dd4166c625966bf95
R. Martinho Fernandes <rmf@rmf.io>
2025-04-09 16:47:52 +0200

    Beginner Mode sets all parameters

--- bxb/setup_ui.lua
+++ bxb/setup_ui.lua
@@ -4,23 +4,59 @@ return function(load)
             UI.setAttribute('setupPanel', 'active', 'false')
         else
             beginner = false
-            difficulty = 1
+            difficulty = 2
+            local oldDifficulty, oldMode
             local difficultyNames = {'Easy', 'Medium', 'Hard', 'Expert'}
+
             function onDifficultyChanged(player, value)
-                UI.setAttribute('difficultyText', 'text', 'Difficulty: ' ..
-                                    difficultyNames[tonumber(value)])
+                UI.setAttributes('difficultyText', {
+                    text = 'Difficulty: ' .. difficultyNames[tonumber(value)]
+                })
                 difficulty = tonumber(value)
             end
 
             function onBeginnerChanged(player, value)
                 beginner = value == 'True'
+                if beginner then
+                    oldDifficulty = difficulty
+                    oldMode = gameMode
+                    onDifficultyChanged(player, 1)
+                    onSetAgendaCooperation()
+                else
+                    onDifficultyChanged(player, oldDifficulty)
+                    if oldMode == 'cooperation' then
+                        onSetAgendaCooperation()
+                    elseif oldMode == 'standard' then
+                        onSetAgendaStandard()
+                    elseif oldMode == 'conflict' then
+                        onSetAgendaConflict()
+                    end
+                end
+                UI.setAttributes('difficultySlider', {
+                    value = difficulty,
+                    interactable = not beginner
+                })
+                UI.setAttributes('cooperationToggle', {
+                    isOn = gameMode == 'cooperation',
+                    interactable = not beginner
+                })
+                UI.setAttributes('standardToggle', {
+                    isOn = gameMode == 'standard',
+                    interactable = not beginner
+                })
+                UI.setAttributes('conflictToggle', {
+                    isOn = gameMode == 'conflict',
+                    interactable = not beginner
+                })
             end
 
             function onDifficultyChanged(player, value)
-                UI.setAttribute('difficultyText', 'text', 'Difficulty: ' ..
-                                    difficultyNames[tonumber(value)])
+                UI.setAttributes('difficultyText', {
+                    text = 'Difficulty: ' .. difficultyNames[tonumber(value)]
+                })
                 difficulty = tonumber(value)
             end
+            onDifficultyChanged(nil, difficulty)
 
             gameMode = 'standard'
             function onSetAgendaCooperation()
@@ -28,6 +64,7 @@ return function(load)
             end
             function onSetAgendaStandard() gameMode = 'standard' end
             function onSetAgendaConflict() gameMode = 'conflict' end
+            onSetAgendaStandard()
         end
         local done = {save = function() return {} end}
         return done
